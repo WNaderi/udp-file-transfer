c567771774ea0d9b2041d40e2b4b6a32d7342962
Aria <naderiaria2003@gmail.com>
2026-07-08 14:37:05 -0700

    remove build files

--- cmake/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/arian/cs432/proj-3
-# Build directory: /home/arian/cs432/proj-3/cmake
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
